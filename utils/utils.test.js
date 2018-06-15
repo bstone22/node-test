16d300eb918a43bb795c8358b1754a55fee3faef
Brent <brent@Julies-MacBook-Pro.local>
2018-06-14 23:57:44 -0700

    inital commit

---
+++ utils/utils.test.js
@@ -0,0 +1,7 @@
+const utils = require('./utils');
+
+// first arument - string what should happen
+// second arguemnt -  function
+it('should add two numbers',() => {
+  var results = utils.add(33,11);
+});
